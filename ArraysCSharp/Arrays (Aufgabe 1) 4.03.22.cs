1f03e82aade9ecc0e17454e7911862085be3a166
Tobias Cremer <tobias.c11@gso.schule.koeln>
2022-03-09 11:04:06 +0100

    Bearbeitung fertig

--- ArraysCSharp/Arrays (Aufgabe 1) 4.03.22.cs
+++ ArraysCSharp/Arrays (Aufgabe 1) 4.03.22.cs
@@ -6,11 +6,52 @@ namespace ArraysCSharp
     {
         static void Main(string[] args)
         {
-            string Sehrgut, Gut, Befriedigend, Ausreichend, Mangelhaft, Ungenügend;
+            int Note = 0;
+            int AnzahlSchüler = 0;
+            double Notenschnitt = 0;
+            string name = "TobiasC53";
+
+            Console.WriteLine("Wie viele Schüler sind in ihrer Klasse?");
+            AnzahlSchüler = Convert.ToInt32(Console.ReadLine());
+
+            string[] namen = new string[AnzahlSchüler];
+
+            int[] noten = new int[AnzahlSchüler];
+            int anzahl = 1;
+
+            for (int AnzahlS = 0; AnzahlS < AnzahlSchüler; AnzahlS++)
+            {
 
 
+                Console.WriteLine("Schüler Name {0}", anzahl);
+                anzahl++;
+                name = Console.ReadLine();
 
+                namen[AnzahlS] = name;
 
+                Console.WriteLine("{0}'s Note:", name);
+                Note = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                if (Note >= 0||Note <= 6)
+                {
+                    Notenschnitt = Notenschnitt + Note;
+
+                    for (int Ausgabe = 0; Ausgabe < AnzahlSchüler; Ausgabe++)
+                    {
+                        Console.WriteLine(namen[Ausgabe] + "\tNote:" + noten[Ausgabe]);
+
+                    }
+
+                    Console.WriteLine("Der Notendurchschnitt beträgt:{0:F2}", Notenschnitt / AnzahlSchüler);
+                }
+                if (Note <=-1||Note >= 7)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Die Note {0} existiert nicht bitte versuche es noch einmal", Note);
+                    AnzahlSchüler = 0;
+                }
+            }
 
         }
     }
